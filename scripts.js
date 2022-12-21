85e0c00efa1486d6ad952c520aa6cd04d48ac1e3
Joona Oja <joona.j.i.oja@gmail.com>
2022-12-21 20:50:10 +0200

    numbers showcase implemented. FIXME: animation start when in view.

--- scripts.js
+++ scripts.js
@@ -16,3 +16,62 @@ function mobileMenuDropdownToggle() {
      }
    }
  }
+
+//  animated Numbers
+const counters = document.querySelectorAll('.animatedNumber');
+
+document.addEventListener('DOMContentLoaded', function(){
+   var counters = document.querySelector('#number1');
+})
+window.onscroll = function(){
+   var counters = document.querySelector('#number1');
+   //TOP
+   if(counters.getBoundingClientRect().top == 0){
+      startCountup();
+      removeEventListener;
+  }	
+ }	
+
+
+
+ //BOTTOM
+   // if(someElement.getBoundingClientRect().bottom <= 0){
+   //    console.log("TRIGGER: bottom of div reached.");
+   // alert('TRIGGER: bottom of div reached');
+   // }
+
+
+// Main function
+function startCountup() {
+   // var counters = document.querySelector('#number1');
+      for(let n of counters) {
+   const updateCount = () => {
+      const target = + n.getAttribute('data-target');
+      const count = + n.innerText;
+      const speed = 100000; // change animation speed here
+      const inc = target / speed; 
+      if(count < target) {
+         n.innerText = Math.ceil(count + inc);
+         setTimeout(updateCount, 1);
+      } else {
+         n.innerText = target;
+      }
+   }
+   updateCount();
+   }
+}
+// for(let n of counters) {
+//   const updateCount = () => {
+//     const target = + n.getAttribute('data-target');
+//     const count = + n.innerText;
+//     const speed = 10000000000; // change animation speed here
+//     const inc = target / speed; 
+//     if(count < target) {
+//       n.innerText = Math.ceil(count + inc);
+//       setTimeout(updateCount, 1);
+//     } else {
+//       n.innerText = target;
+//     }
+//   }
+//   updateCount();
+// }
